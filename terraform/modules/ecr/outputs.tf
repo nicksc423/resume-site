54043069fab064def5429eab65e102ce2538a87b
Nick Collins <nicksc423@gmail.com>
2022-07-06 12:41:24 -0700

    first pass at CICD

--- terraform/modules/ecr/outputs.tf
+++ terraform/modules/ecr/outputs.tf
@@ -7,3 +7,8 @@ output "image" {
   description = "The image object for our lambda"
   value       = data.aws_ecr_image.lambda_image
 }
+
+resource "local_file" "ECR_URL" {
+    filename = "../out/ECR_URL"
+    content = aws_ecr_repository.repo.repository_url
+}
